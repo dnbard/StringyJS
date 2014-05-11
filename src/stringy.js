4bcd09d296188688cac27895131a028771a069b2
Alex Bardanov <dnbard@gmail.com>
2014-05-11 22:29:53 +0300

    #template accepting {custom} placeholders

--- src/stringy.js
+++ src/stringy.js
@@ -1,26 +1,26 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
         define([], factory);
     } else if (typeof exports === 'object') {
-        // Node. Does not work with strict CommonJS, but
-        // only CommonJS-like environments that support module.exports,
-        // like Node.
         module.exports = factory();
     } else {
-        // Browser globals (root is window)
         root.Stringy = factory();
   }
 }(this, function () {
     return {
         template: function(format){
-            var args = Array.prototype.slice.call(arguments, 1);
-            var sprintfRegex = /\{(\d+)\}/g;
- 
-            var sprintf = function (match, number) {
-                return number in args ? args[number] : match;
+            var args = {};
+            if (typeof arguments[1] === 'object'){
+                args = arguments[1];
+            } else if (arguments[1]) {
+                args = Array.prototype.slice.call(arguments, 1);
+            }
+
+            var sprintfRegex = /\{([^{]+?)\}/g; 
+            var sprintf = function (match, value) {
+                return value in args ? args[value] : match;
             };
- 
+
             return format.replace(sprintfRegex, sprintf);
         }
     };
--- test/spec/test.js
+++ test/spec/test.js
@@ -15,18 +15,53 @@
                 expect(Stringy.template).to.be.a('function');
             });
 
-            it('should return same string without format', function () {
+            it('should return same string without template', function () {
                 var str = 'qwerty';
                 expect(Stringy.template(str)).to.be.equal(str);
             });
 
-            it('should format by given string', function(){
+            it('should format string by given template', function(){
                 var input = '{0} qwerty {1}',
                     param0 = 'abc',
                     param1 = 'def',
                     result = 'abc qwerty def';
 
                 expect(Stringy.template(input, param0, param1)).to.be.equal(result);
+            });
+
+            it('should format string using {custom} templating', function () {
+                var input = '{first} qwerty {second}',
+                    args = {
+                        'first': 'abc',
+                        'second': 'def'
+                    },
+                    result = 'abc qwerty def';
+
+                expect(Stringy.template(input, args)).to.be.equal(result);
+            });
+
+            it('should not fill template placeholder whem argument is undefined', function () {
+                var input = '{first} qwerty {second}',
+                    result = input;
+                expect(Stringy.template(input)).to.be.equal(result);
+            });
+
+            it('should not fill template placeholder when argument is null', function () {
+                var input = '{first} qwerty {second}',
+                    result = input;
+                expect(Stringy.template(input)).to.be.equal(result);
+            });
+
+            it('should not fill template placeholder when argument is empty object', function () {
+                var input = '{first} qwerty {second}',
+                    result = input;
+                expect(Stringy.template(input, {})).to.be.equal(result);
+            });
+
+            it('should not fill template placeholder when argument is empty array', function () {
+                var input = '{first} qwerty {second}',
+                    result = input;
+                expect(Stringy.template(input)).to.be.equal(result);
             });
         })
     });
